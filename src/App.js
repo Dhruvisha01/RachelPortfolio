7878d9a6d2454943d61c41cd8e111a4c5d79bd17
Dhruvisha Mondhe <63810182+Dhruvisha01@users.noreply.github.com>
2025-04-21 21:23:09 -0400

    Resolved error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
