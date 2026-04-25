682929ae841aa636ab73b5403ae99c8f7aa2f892
“Rachel <rachsadeh@gmail.com>
2026-04-24 23:39:12 -0400

    Refactor App component by removing unused state and imports, and cleaning up commented code.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from "./logo.svg";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -11,26 +11,16 @@ import TimeToMoveCaseStudy from "./pages/TimeToMoveCaseStudy";
 import ProofpointCaseStudy from "./pages/ProofpointCaseStudy";
 import BikeLinkProCaseStudy from "./pages/BikeLinkProCaseStudy";
 import ClientViewCaseStudy from "./pages/ClientViewCaseStudy";
-/*import CustomCursorKinet from "./components/CustomCursorKinet";*/
-// import CustomCursor from "./components/customCursor";
-import CustomCursorKinet from "./components/customCursorKinet";
 function App() {
-  const [showCustomCursor, setShowCustomCursor] = useState(false);
   useEffect(() => {
     fetch("https://rachelportfoliobackend.onrender.com/")
       .then((res) => res.text())
       .then((data) => console.log(data));
-
-    const isMouseDevice = window.matchMedia("(hover: hover) and (pointer: fine)").matches;
-    setShowCustomCursor(isMouseDevice);
   }, []);
 
   return (
     <Router>
       <div className="app">
-        {/* <CustomCursorKinet /> */}
-        {/* <CustomCursor /> */}
-        {showCustomCursor && <CustomCursorKinet />}
         {/* <Header /> */}
         <Routes>
           <Route path="/" element={<Home />} />
